fe03998b79bc01604d5af8555e45ca08de7cc3ae
justinebedes <jebedes@gmail.com>
2025-11-07 16:36:45 +0800

    Call backend

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    fetch('http://backend.ebedes.com/seinfeldGPT')
+      .then(response => response.text())
+      .then(text => setData(text))
+      .catch(error => setData('Error: ' + error.message));
+  }, []);
+
+  return <div>{data ? data : 'Loading...'}</div>;
 }
 
 export default App;
